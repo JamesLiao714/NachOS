0ab896dc82c4bbdd9164e8e6811b58eb65e00796
james <s9446760133@gmail.com>
2019-10-25 08:42:37 +0800

    delete

--- code/test/test1.c
+++
@@ -1,7 +0,0 @@
-#include "syscall.h"
-main()
-	{
-		int	n;
-		for (n=9;n>5;n--)
-			PrintInt(n);
-	}
--- code/test/test2.c
+++
@@ -1,8 +0,0 @@
-#include "syscall.h"
-
-main()
-        {
-                int     n;
-                for (n=20;n<=25;n++)
-                        PrintInt(n);
-        }
